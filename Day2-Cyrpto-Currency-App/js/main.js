367f96ee7af914d91dc29fcf4de08ca749d97b8f
safaanilatasoy <safaanilatasoy@gmail.com>
2023-07-22 15:58:34 +0300

    countries js dosyası eklendi

---
+++ Day2-Cyrpto-Currency-App/js/main.js
@@ -0,0 +1,15 @@
+const fromCur = document.querySelector(".from select");
+const toCur = document.querySelector(".to select");
+const getButton = document.querySelector("form button");
+const exIcon = document.querySelector("form .reverse");
+const amount = document.querySelector("form input");
+const exRateTxt = document.querySelector("form .result");
+
+// Event listener for current dropdowns (select)
+
+
+[fromCur, toCur].forEach(select, i) => {
+    for(let curCode in Country_List){
+        
+    }
+}

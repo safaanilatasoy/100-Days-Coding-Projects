91c5d837a0da00d61632abcb935e015e6ced18e4
safaanilatasoy <safaanilatasoy@gmail.com>
2023-07-22 16:27:24 +0300

    Ülke isimleri ve bayrakları select inputuna eklendi

--- Day2-Cyrpto-Currency-App/js/main.js
+++ Day2-Cyrpto-Currency-App/js/main.js
@@ -1,15 +1,20 @@
 const fromCur = document.querySelector(".from select");
 const toCur = document.querySelector(".to select");
-const getButton = document.querySelector("form button");
+const getBtn = document.querySelector("form button");
 const exIcon = document.querySelector("form .reverse");
 const amount = document.querySelector("form input");
 const exRateTxt = document.querySelector("form .result");
 
-// Event listener for current dropdowns (select)
+// Event listener for currency dropdowns (select)
 
-
-[fromCur, toCur].forEach(select, i) => {
-    for(let curCode in Country_List){
-        
+[fromCur, toCur].forEach((select, i) => {
+    for (let curCode in Country_List) {
+        const selected = (i === 0 && curCode === "USD") || (i === 1 && curCode === "GBP") ? "selected" : "";
+        select.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
     }
-}
+    select.addEventListener("change", () => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+    });
+});
